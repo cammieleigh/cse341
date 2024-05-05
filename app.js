56f0d2f6f57cd33a36a5c02285f761a1e5f0b7b8
cammieleighp <cammie.peterson18@gmail.com>
2024-05-04 23:33:08 -0600

    mongo

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Cammie Lee");
-});
+const mongodb = require('./db/connect');
+const bodyParser = require('body-parser');
 
-app.get('/Justin', (req, res) => {
-    res.send("Justin Lee");
-  });
- 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+const port = process.env.PORT || 8080;
+
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./Routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
