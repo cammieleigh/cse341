d8feb654e492a73ca3c547d92bd866a0b01d11d1
cammieleighp <cammie.peterson18@gmail.com>
2024-05-11 20:56:41 -0600

    week3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -24,4 +24,53 @@ const getSingle = async (req, res, next) => {
     });
   };
 
-module.exports = { getAll, getSingle };
+
+const createContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    birthdate: req.body.birthdate,
+    faveColor: req.body.faveColor,
+  }
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+}
+
+const updateContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const updatedContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    birthdate: req.body.birthdate,
+    faveColor: req.body.faveColor,
+  }
+  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId }, updatedContact);
+  if (response.acknowledged) {
+    res.status(204).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+}
+
+const deleteContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .remove({ _id: userId }, true);
+      if (response.acknowledged) {
+        res.status(200).json(response);
+      } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+      }
+}
+
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
